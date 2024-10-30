acd1d2cc5b23b11cc5dec94a6fbeb7abcbe49f29
Alef David <david.eszdiaz@gmail.com>
2024-10-30 00:28:29 -0600

    Inicializando proyecto

---
+++ cine2doseg/Controllers/SucursalController.cs
@@ -0,0 +1,96 @@
+﻿pusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//-------------------------------
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using cine2doseg.Models;
+//-------------------------------
+
+namespace cine2doseg.Controllers
+{
+    public class SucursalController : ApiController
+    {
+
+        [HttpGet] //estas son directivas
+        [Route("cine/sucursal/vwrptsucursales")]
+        public DataSet vwRptsucursales()
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                // Creacion del objeto usuario para la insercion 
+                clsSucursal objUsuario = new clsSucursal();
+
+                //Ejecucion del metodo de insercion y recepion de resultados
+                ds = objUsuario.vwRptsucursales();
+            }
+            catch (Exception ex)
+            {
+                //Configurar el DataSet para salida
+                //(Formateo con clsApiStatus
+                DataTable dt = new DataTable("vwRptsucursales");
+                dt.Columns.Add("statusExec");
+                dt.Columns.Add("msg");
+                dt.Columns.Add("ban");
+                dt.Columns.Add("msgData");
+                dt.Columns.Add("msgException");
+                //Formateo de los datos de salida
+                DataRow dr = dt.NewRow();
+                dr["statusExec"] = "false";
+                dr["msg"] = "Error en reporte de ususaios, verificar ...";
+                dr["ban"] = "-1";
+                dr["msgData"] = ex.Message.ToString();
+                dr["msgException"] = ex.InnerException.ToString();
+                //asignar datos de salida
+                dt.Rows.Add(dr);
+                ds.Tables.Add(dt);
+
+            }
+            //Return del DataSet con los datos recibidos
+            // (o formateados dentro del catch
+            return ds;
+        }
+
+
+        [HttpPost]
+        [Route("cine/sucursal/spinssucursales")]
+        public clsApiStatus spInsSucursales([FromBody] clsSucursal modelo)
+        {
+            clsApiStatus objRespuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+            try
+            {
+                // Creacion del objeto usuario para la insercion 
+                clsSucursal objSucursal = new clsSucursal(modelo.nombre,
+                                                       modelo.direccion,
+                                                       modelo.url,
+                                                       modelo.logo);
+                DataSet ds = new DataSet();
+                //Ejecucion del metodo de insercion y recepion de resultados
+                ds = objSucursal.spInsSucursales();
+                //Configuracion de los atributos de salida
+                objRespuesta.statusExec = true;
+                objRespuesta.msg = "Registro de usuario exitoso (cine)";
+                objRespuesta.ban = int.Parse(ds.Tables[0].Rows[0][0].ToString());
+                jsonResp.Add("msgData", "Registro de usuario exitoso (cine)");
+                objRespuesta.datos = jsonResp;
+
+            }
+            catch (Exception ex)
+            {
+                objRespuesta.statusExec = false;
+                objRespuesta.msg = "Registro de usuario fallido (cine)";
+                objRespuesta.ban = -1;
+                jsonResp.Add("msgData", ex.Message.ToString());
+                objRespuesta.datos = jsonResp;
+            }
+            return objRespuesta;
+        }
+
+    }
+}
---
+++ cine2doseg/Models/clsSucursal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//--------
+using System.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using MySql.Data.MySqlClient;
+using System.Data;
+//-------
+
+namespace cine2doseg.Models
+{
+    public class clsSucursal
+    {
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string url { get; set; }
+
+        public string logo { get; set; }
+
+
+        public clsSucursal()
+        {
+
+        }
+
+        public clsSucursal(string nombre, string direccion, string url,string logo)
+        {
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.url = url;
+            this.logo = logo;
+
+
+        }
+
+        private string cadCnn = ConfigurationManager.
+            ConnectionStrings["CINE"].
+            ConnectionString;
+
+
+        public DataSet vwRptsucursales()
+        {
+            // Crear el comando SQL
+            string cadSQL = "";
+            cadSQL = "SELECT * FROM vwrptsucursales;";
+            // Configuracion de objetos de conexion
+            MySqlConnection cnn = new MySqlConnection(cadCnn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cadSQL, cadCnn); //da es por datadatos
+            DataSet ds = new DataSet();
+            //ejecucion y salida
+            da.Fill(ds, "vwRptsucursales"); // <----------------
+            return ds;
+            //ds es igual al dataset
+
+        }
+
+
+        public DataSet spInsSucursales()
+        {
+            // Crear el comando SQL
+
+            string cadSQL = "";
+            cadSQL = "CALL spInsSucursales('" + this.nombre + "', '" + this.direccion + "', '" + this.url + "', '" + this.logo + "');";
+
+            // Configuracion de objetos de conexion
+            MySqlConnection cnn = new MySqlConnection(cadCnn);  
+            MySqlDataAdapter da = new MySqlDataAdapter(cadSQL, cadCnn); //da es por datadatos
+            DataSet ds = new DataSet();
+
+            //ejecucion y salida
+            da.Fill(ds, "spInsSucursales"); // <----------------
+            return ds;
+        }
+
+    }
+    
+}
+   
